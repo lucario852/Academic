0bdaee912e69563e721f65ea16da5b5bdfd7b477
lucario852 <william.mendoza@ustabuca.edu.co>
2021-11-09 21:34:40 -0500

    Diseño_Pantalla_Principal

--- app/src/main/java/com/example/academic/MainActivity.java
+++ app/src/main/java/com/example/academic/MainActivity.java
@@ -1,14 +1,59 @@
 package com.example.academic;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public CardView notas,estudiante,curso,ajustes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        notas = (CardView) findViewById(R.id.n);
+        estudiante = (CardView) findViewById(R.id.e);
+        curso = (CardView) findViewById(R.id.c);
+        ajustes = (CardView) findViewById(R.id.a);
+
+        notas.setOnClickListener(this);
+        estudiante.setOnClickListener(this);
+        curso.setOnClickListener(this);
+        ajustes.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        Intent i;
+
+        switch (v.getId())
+        {
+            case R.id.n:
+                i = new Intent(this,Activity_Notas.class);
+                startActivity(i);
+                break;
+
+            case R.id.e:
+                i = new Intent(this,Activity_Estudiante.class);
+                startActivity(i);
+                break;
+
+            case R.id.c:
+                i = new Intent(this,Activity_Curso.class);
+                startActivity(i);
+                break;
+
+            case R.id.a:
+                i = new Intent(this,Activity_Ajuste.class);
+                startActivity(i);
+                break;
+        }
     }
 }
